f91559e69973c6179ef5c460d8f8cd7111b99888
Pedro Javier Martos Velasco <pjmv1986@gmail.com>
2018-04-05 03:05:16 +0200

    Una célula viva es un booleano true

---
+++ src/test/java/es/pjmartos/GameOfLifeTest.java
@@ -0,0 +1,16 @@
+package es.pjmartos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class GameOfLifeTest {
+
+    @Test
+    public void anAliveCellShouldExist() {
+        boolean aliveCell = true;
+
+        assertTrue(aliveCell);
+    }
+
+}
